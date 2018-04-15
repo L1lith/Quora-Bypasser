6dfe7f915388f0b0abda59f522eb6c373472c183
L1lith <lily@lillith.pw>
2018-04-15 14:29:44 -0600

    remove log statements

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const bannedBodyClasses = ["signup_wall_prevent_scroll", "logged_out"]
 
 function run(){
   const {body} = document
-  console.log('Bypassing')
   const signupWalls = Array.from(body.childNodes).filter(node => (node.id || "").endsWith('signup_wall_wrapper'))
   if (signupWalls.length > 0) signupWalls.forEach(wall => body.removeChild(wall))
-  console.log(body.className)
   let filteredBodyClass = body.className
   bannedBodyClasses.forEach(className => filteredBodyClass = filteredBodyClass.replace(className, ""))
   if (body.className !== filteredBodyClass) body.className = filteredBodyClass
